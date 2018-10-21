3db5f3d80ef59f23cd0925998cbf41133fe1f289
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2018-10-21 12:45:31 -0500

    Se completo el archivo GuiceContextListener en el cual quedaron las injecciones con los bind adecuados para el trabajo, se creo una carpeta view en la cual se incluyo el archivo BasePageBean

--- MyBatis_Introduction_VideoRental/src/main/java/edu/eci/pdsw/guice/GuiceContextListener.java
+++ MyBatis_Introduction_VideoRental/src/main/java/edu/eci/pdsw/guice/GuiceContextListener.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.pdsw.guice;
 
-/**
- *
- * @author jfmor
- */
-public class GuiceContextListener {
-    
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import edu.eci.pdsw.sampleprj.dao.ClienteDAO;
+import edu.eci.pdsw.sampleprj.dao.ItemDAO;
+import edu.eci.pdsw.sampleprj.dao.ItemRentadoDAO;
+import edu.eci.pdsw.sampleprj.dao.TipoItemDAO;
+import edu.eci.pdsw.sampleprj.dao.mybatis.MyBATISClienteDAO;
+import edu.eci.pdsw.sampleprj.dao.mybatis.MyBATISItemDAO;
+import edu.eci.pdsw.sampleprj.dao.mybatis.MyBATISItemRentadoDAO;
+import edu.eci.pdsw.sampleprj.dao.mybatis.MyBATISTipoItemDAO;
+import edu.eci.pdsw.samples.services.ServiciosAlquiler;
+import edu.eci.pdsw.samples.services.impl.ServiciosAlquilerImpl;
+import edu.eci.pdsw.samples.services.impl.ServiciosAlquilerItemsStub;
+
+public class GuiceContextListener implements ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.removeAttribute(Injector.class.getName());
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Injector injector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.MySQL);
+                setEnvironmentId("development");
+                setClassPathResource("mybatis-config.xml");
+
+                // TODO Add service class associated to Stub implementation
+                // Servicio
+                bind(ServiciosAlquiler.class).to(ServiciosAlquilerItemsStub.class);
+                // Cliente
+                bind(ClienteDAO.class).to(MyBATISClienteDAO.class);
+                //Item
+                bind(ItemDAO.class).to(MyBATISItemDAO.class);      
+                //bind(ItemRentadoDAO.class).to(MyBATISItemRentadoDAO.class);
+                bind(TipoItemDAO.class).to(MyBATISTipoItemDAO.class);
+            }
+        });
+
+        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
+    }
 }
